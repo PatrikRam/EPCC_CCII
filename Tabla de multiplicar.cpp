84e6c84d57e1e683a95e4c793b07517418d431f1
PatrikRam <pramirezza@unsa.edu.pe>
2022-04-27 22:29:18 -0500

    teoria3trab

---
+++ Tabla de multiplicar.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<" *  ";
+	for (int i=1; i<=10; i++){
+		cout<<i<<"\t";
+		
+	}
+	cout<<"\n----------------------------------------------------------------------------";
+	cout<<endl;
+	cout<<" 1| ";
+	for (int i=1; i<=10; i++){
+		cout<<i<<"\t";
+	}
+	cout<<endl;
+	cout<<" 2| ";
+	for (int i=2; i<=20; i=i+2){
+		cout<<i<<"\t";
+	}
+	cout<<endl;
+	cout<<" 3| ";
+	for (int i=3; i<=30; i=i+3){
+		cout<<i<<"\t";
+	}
+	cout<<endl;
+	cout<<" 4| ";
+	for (int i=4; i<=40; i=i+4){
+		cout<<i<<"\t";
+	}
+	cout<<endl;
+	cout<<" 5| ";
+	for (int i=5; i<=50; i=i+5){
+		cout<<i<<"\t";
+	}
+	cout<<endl;
+	cout<<" 6| ";
+	for (int i=6; i<=60; i=i+6){
+		cout<<i<<"\t";
+	}
+	cout<<endl;
+	cout<<" 7| ";
+	for (int i=7; i<=70; i=i+7){
+		cout<<i<<"\t";
+	}
+	cout<<endl;
+	cout<<" 8| ";
+	for (int i=8; i<=80; i=i+8){
+		cout<<i<<"\t";
+	}
+	cout<<endl;
+	cout<<" 9| ";
+	for (int i=9; i<=90; i=i+9){
+		cout<<i<<"\t";
+	}
+	cout<<endl;
+	cout<<"10|";
+	for (int i=10; i<=100; i=i+10){
+		cout<<i<<"\t";
+	}
+	cout<<endl;
+	
+
+	cout<<endl;
+	
+	
+	
+}
